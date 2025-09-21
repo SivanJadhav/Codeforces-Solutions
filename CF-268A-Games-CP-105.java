547785d91947fd71a3ffa08d9c91e625e0312d0e
Sivan <sivanjaadhav@gmail.com>
2025-09-21 11:21:19 +0530

    CP-105 #done solve games

---
+++ CF-268A-Games-CP-105.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // Switch between console and file IO
+    static boolean useFileIO = false; // set true for USACO-style file IO
+    static FastReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        if (useFileIO) {
+            in = new FastReader(new File("input.txt"));
+            out = new PrintWriter(new File("output.txt"));
+        } else {
+            in = new FastReader();
+            out = new PrintWriter(System.out);
+        }
+
+        int n = in.nextInt();
+
+        int[][] games = new int[n][2];
+        int cases = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                games[i][j] = in.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (games[i][1] == games[j][0]) {
+                    cases++;
+                }
+            }
+        }
+        
+        out.println(cases);
+
+        out.close();
+    }
+
+    // FastReader for both console and file
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        // Console
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        // File
+        public FastReader(File f) throws IOException {
+            br = new BufferedReader(new FileReader(f));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            st = null; // discard leftover tokens
+            return br.readLine();
+        }
+    }
+}
