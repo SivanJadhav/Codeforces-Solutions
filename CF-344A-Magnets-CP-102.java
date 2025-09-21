ab031c586448e819d032fb93424bdf2473692a45
Sivan <sivanjaadhav@gmail.com>
2025-09-21 16:54:09 +0530

    Update txt with Jira tasks

--- CF-344A-Magnets-CP-102.java
+++ CF-344A-Magnets-CP-102.java
@@ -3,80 +3,81 @@ import java.util.*;
 
 public class Main {
 
-    // Switch between console and file IO
-    static boolean useFileIO = false; // set true for USACO-style file IO
-    static FastReader in;
-    static PrintWriter out;
+	// Switch between console and file IO
+	static boolean useFileIO = false; // set true for USACO-style file IO
+	static FastReader in;
+	static PrintWriter out;
 
-    public static void main(String[] args) throws IOException {
-        if (useFileIO) {
-            in = new FastReader(new File("input.txt"));
-            out = new PrintWriter(new File("output.txt"));
-        } else {
-            in = new FastReader();
-            out = new PrintWriter(System.out);
-        }
+	public static void main(String[] args) throws IOException {
+		if (useFileIO) {
+			in = new FastReader(new File("input.txt"));
+			out = new PrintWriter(new File("output.txt"));
+		} else {
+			in = new FastReader();
+			out = new PrintWriter(System.out);
+		}
 
-        // ========== YOUR CODE STARTS HERE ==========
-        int n = in.nextInt();
-        int groups = 0;
-        String crr = "";
+		// ========== YOUR CODE STARTS HERE ==========
+		int n = in.nextInt();
+		int groups = 0;
+		String crr = "";
 
-        for (int i = 0; i < n; i++) {
-            String magnet = in.nextLine();
+		for (int i = 0; i < n; i++) {
+			String magnet = in.nextLine();
 
-            if (crr.equals(magnet) == false) {
-                crr = magnet;
-                groups++;
-            }
-        }
+			if (crr.equals(magnet) == false) {
+				crr = magnet;
+				groups++;
+			}
+		}
 
-        out.println(groups);
-        // ========== YOUR CODE ENDS HERE ============
+		out.println(groups);
+		// ========== YOUR CODE ENDS HERE ============
 
-        out.close();
-    }
+		out.close();
+	}
 
-    // FastReader for both console and file
-    static class FastReader {
+	// FastReader for both console and file
+	static class FastReader {
 
-        BufferedReader br;
-        StringTokenizer st;
+		BufferedReader br;
+		StringTokenizer st;
 
-        // Console
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
+		// Console
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        // File
-        public FastReader(File f) throws IOException {
-            br = new BufferedReader(new FileReader(f));
-        }
+		// File
+		public FastReader(File f) throws IOException {
+			br = new BufferedReader(new FileReader(f));
+		}
 
-        String next() throws IOException {
-            while (st == null || !st.hasMoreTokens()) {
-                String line = br.readLine();
-                if (line == null) return null;
-                st = new StringTokenizer(line);
-            }
-            return st.nextToken();
-        }
+		String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				String line = br.readLine();
+				if (line == null)
+					return null;
+				st = new StringTokenizer(line);
+			}
+			return st.nextToken();
+		}
 
-        int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
+		int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
 
-        long nextLong() throws IOException {
-            return Long.parseLong(next());
-        }
+		long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
 
-        double nextDouble() throws IOException {
-            return Double.parseDouble(next());
-        }
+		double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		}
 
-        String nextLine() throws IOException {
-            st = null; // discard leftover tokens
-            return br.readLine();
-        }
-    }
+		String nextLine() throws IOException {
+			st = null; // discard leftover tokens
+			return br.readLine();
+		}
+	}
 }
