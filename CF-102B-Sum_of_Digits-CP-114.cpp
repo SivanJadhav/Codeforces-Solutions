f79267d5b3162360efaa4d9975a7f7c62fc72d72
Sivan <sivanjaadhav@gmail.com>
2025-09-21 19:51:01 +0530

    CP-114 #in-progress start solving sum of digits

---
+++ CF-102B-Sum_of_Digits-CP-114.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string number = "";
+  cin >> number;
+
+  int spells = 0;
+
+  while (number.length() != 1) {
+    spells++;
+    int len = number.length();
+    long long sum = 0;
+    for (int i = 0; i < len; i++) {
+      sum += number[i] - '0';
+    }
+    number = to_string(sum);
+  }
+
+  cout << spells << '\n';
+}
