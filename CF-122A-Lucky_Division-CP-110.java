31808292da029edc06c310eca1fae4e8e9a8fa49
Sivan <sivanjaadhav@gmail.com>
2025-09-21 14:16:00 +0530

    CP-110 #done solve lucky division

---
+++ CF-122A-Lucky_Division-CP-110.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // Switch between console and file IO
+    static boolean useFileIO = false; // set true for USACO-style file IO
+    static FastReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        if (useFileIO) {
+            in = new FastReader(new File("input.txt"));
+            out = new PrintWriter(new File("output.txt"));
+        } else {
+            in = new FastReader();
+            out = new PrintWriter(System.out);
+        }
+
+        int n = in.nextInt();
+        boolean is_lucky = false;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                String lucky = Integer.toString(i);
+                is_lucky = true;
+                for (int j = 0, len = lucky.length(); j < len; j++) {
+                    if (lucky.charAt(j) != '4' && lucky.charAt(j) != '7') {
+                        is_lucky = false;
+                    }
+                }
+                if (is_lucky == true) {
+                    out.println("YES");
+                    out.flush();
+                    return;
+                }
+            }
+        }
+
+        out.println("NO");
+
+        out.close();
+    }
+
+    // FastReader for both console and file
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        // Console
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        // File
+        public FastReader(File f) throws IOException {
+            br = new BufferedReader(new FileReader(f));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                String line = br.readLine();
+                if (line == null) return null;
+                st = new StringTokenizer(line);
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            st = null; // discard leftover tokens
+            return br.readLine();
+        }
+    }
+}
